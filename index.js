368a8ff030bc2e565e696febf09c63a17b78e55f
chrischichester <38088078+chrischichester@users.noreply.github.com>
2018-04-05 18:47:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -540,9 +540,9 @@ var request = require("request");
 var augustState = (state == Characteristic.LockTargetState.SECURED) ? "lock" : "unlock";
 console.log(augustState);
   var status = self.lockState[state];
-    self.platformLog("543 "+ self.lockids[1]);
+    self.platformLog("543 "+ self.lockids[0]+ " " + augustState);
     request.put({
-          url: "https://api-production.august.com/remoteoperate/" + self.lockids[2] + "/" + augustState,
+          url: "https://api-production.august.com/remoteoperate/" + self.lockids[0] + "/" + augustState,
           "headers": {
               "Content-Type": 'application/json',
               'x-kease-api-key': '14445b6a2dba',
